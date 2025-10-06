8cdbc17e5de67cb3be98d48fb4154208fb416a5d
Sally <143855164+SallyCunningham620@users.noreply.github.com>
2025-10-06 14:20:16 -0500

    add fonts, picture, btnActive

---
+++ src/btnActive.js
@@ -0,0 +1,9 @@
+function setBtnActive(id) {
+  const activeBtn = document.querySelector('.tab.active');
+  if (activeBtn) activeBtn.classList.remove('active');
+
+  const homeBtn = document.getElementById(id);
+  homeBtn.classList.add('active');
+}
+
+export default setBtnActive;
