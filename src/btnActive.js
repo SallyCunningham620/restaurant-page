2e91341316da7c9c479c1a52a06d527dddce57cd
Sally <143855164+SallyCunningham620@users.noreply.github.com>
2025-10-07 11:03:35 -0500

    Deployment commit

--- src/btnActive.js
+++
@@ -1,9 +0,0 @@
-function setBtnActive(id) {
-  const activeBtn = document.querySelector('.tab.active');
-  if (activeBtn) activeBtn.classList.remove('active');
-
-  const homeBtn = document.getElementById(id);
-  homeBtn.classList.add('active');
-}
-
-export default setBtnActive;
